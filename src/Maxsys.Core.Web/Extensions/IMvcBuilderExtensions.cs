1f4f05667aea2eed64f8f61620ddd8e42874843d
Maxsys Tech <maxsystech@outlook.com>
2025-02-10 12:27:40 -0300

    refactor(web): Add JsonStringEnumConverter em JSON_DEFAULT_OPTIONS ao configurar IMvcBuilder

--- src/Maxsys.Core.Web/Extensions/IMvcBuilderExtensions.cs
+++ src/Maxsys.Core.Web/Extensions/IMvcBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.Extensions.DependencyInjection;
 using static Maxsys.Core.Extensions.JsonExtensions;
 
@@ -9,17 +10,24 @@ public static class IMvcBuilderExtensions
     /// <summary>
     /// Atalho para IMvcBuilder.AddJsonOptions(options).<br/>
     /// Configuração para evitar <see cref="JsonException"/> (object cycle), ignorar case e não escrever properties nulas.
+    /// Adicionar <see cref="JsonStringEnumConverter"/> a <see cref="JSON_DEFAULT_OPTIONS"/>.
     /// </summary>
     public static IMvcBuilder ConfigureJsonOptions(this IMvcBuilder builder)
     {
+        JSON_DEFAULT_OPTIONS.Converters.Add(new JsonStringEnumConverter());
+
         builder.AddJsonOptions(options =>
         {
-            options.JsonSerializerOptions.DefaultIgnoreCondition = JSON_DEFAULT_OPTIONS.DefaultIgnoreCondition;
+            options.JsonSerializerOptions.DefaultIgnoreCondition = .DefaultIgnoreCondition;
             options.JsonSerializerOptions.ReferenceHandler = JSON_DEFAULT_OPTIONS.ReferenceHandler;
             options.JsonSerializerOptions.PropertyNameCaseInsensitive = JSON_DEFAULT_OPTIONS.PropertyNameCaseInsensitive;
+            options.JsonSerializerOptions.PropertyNamingPolicy = JSON_DEFAULT_OPTIONS.PropertyNamingPolicy;
 
             // Exibir Enum literal no swagger: https://stackoverflow.com/a/65318486
-            //options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            foreach (var converter in JSON_DEFAULT_OPTIONS.Converters)
+            {
+                options.JsonSerializerOptions.Converters.Add(converter);
+            }
         });
 
         return builder;
