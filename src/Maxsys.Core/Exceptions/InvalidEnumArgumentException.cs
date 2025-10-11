0ed8f8a20c284d7366a0e087b7e5cf1f0774dcca
Maxsys <maxsystech@outlook.com>
2025-10-11 15:03:03 -0300

    feat: adicionado InvalidEnumArgumentException

---
+++ src/Maxsys.Core/Exceptions/InvalidEnumArgumentException.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Maxsys.Core.Exceptions;
+
+public class InvalidEnumArgumentException<TEnum> : InvalidEnumArgumentException where TEnum : struct, Enum
+{
+    public InvalidEnumArgumentException(TEnum value, [CallerArgumentExpression(nameof(value))] string argumentName = "")
+        : base(argumentName, Convert.ToInt32(value), typeof(TEnum))
+    { }
+}
