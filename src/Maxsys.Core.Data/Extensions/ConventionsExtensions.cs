1e70613da177e154bb8943b2c32dccae0d64a91b
Maxsys <maxsystech@outlook.com>
2025-10-11 17:19:15 -0300

    feat: Adicionado ConventionsExtensions

---
+++ src/Maxsys.Core.Data/Extensions/ConventionsExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Maxsys.Core.EventSourcing;
+using Microsoft.EntityFrameworkCore;
+
+namespace Maxsys.Core.Data.Extensions;
+
+public static class ConventionsExtensions
+{
+    /// <summary>
+    /// Use:
+    /// <code>
+    /// protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    /// {
+    ///     configurationBuilder.StringToVarcharConvention(100);
+    ///
+    ///     base.ConfigureConventions(configurationBuilder);
+    /// }
+    /// </code>
+    /// </summary>
+    public static ModelConfigurationBuilder StringToVarcharConvention(this ModelConfigurationBuilder configurationBuilder, int maxLength = -1)
+    {
+        configurationBuilder.Properties<string>(x => x.AreUnicode(false).HaveMaxLength(maxLength));
+
+        return configurationBuilder;
+    }
+
+    /// <summary>
+    /// Use:
+    /// <code>
+    /// protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    /// {
+    ///     configurationBuilder.IgnoreDomainEventConvention();
+    ///
+    ///     base.ConfigureConventions(configurationBuilder);
+    /// }
+    /// </code>
+    /// </summary>
+    public static ModelConfigurationBuilder IgnoreDomainEventConvention(this ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.IgnoreAny<DomainEvent>();
+
+        return configurationBuilder;
+    }
+}
