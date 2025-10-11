ac3da680394b13e18ffe5712647c148ea274ce03
Maxsys <maxsystech@outlook.com>
2025-10-11 17:41:30 -0300

    refactor: Exception renomeada: `NotInitializedWorkbookException` -> `WorkbookNotInitializedException`;

--- src/Maxsys.Core.Excel/Exceptions/WorkbookNotInitializedException.cs
+++ src/Maxsys.Core.Excel/Exceptions/WorkbookNotInitializedException.cs
@@ -5,14 +5,14 @@ namespace Maxsys.Core.Excel.Exceptions;
 /// <summary>
 /// Representa um erro que ocorre ao tentar ler um <see cref="XLWorkbook"/> não inicializado.
 /// </summary>
-public sealed class NotInitializedWorkbookException : Exception
+public sealed class WorkbookNotInitializedException : Exception
 {
     private const string DEFAULT_MESSAGE = "Not initialized Workbook. Method Initialize(Stream) must be called before use XLWorkbook property.";
 
     /// <summary>
     /// Representa um erro que ocorre ao tentar ler um <see cref="XLWorkbook"/> não inicializado.
     /// </summary>
-    public NotInitializedWorkbookException()
+    public WorkbookNotInitializedException()
       : base(DEFAULT_MESSAGE)
     { }
 }
